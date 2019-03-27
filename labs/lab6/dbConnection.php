7901832969e1c65a9639bfb45783ad9a9a4119a6
Chris Vitale <cvitale@csumb.edu>
2019-03-27 02:15:16 +0000

    Lab 6

--- labs/lab6/dbConnection.php
+++ labs/lab6/dbConnection.php
@@ -5,7 +5,7 @@ function getDatabaseConnection($dbname = 'ottermart'){
     $password = '';
     
     
-        if  (strpos($_SERVER['HTTP_HOST'], 'heroku') !== false) {
+        if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
         $url = parse_url(getenv("mysql://be4028c0a5d88f:4840c9b0@us-cdbr-iron-east-03.cleardb.net/heroku_e668be4425805cc?reconnect=true"));
         $host = $url["host"];
         $dbname = substr($url["path"], 1);
