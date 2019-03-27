a4c0017c14dcc494fb1a5382b5ace86c86134277
Chris Vitale <cvitale@csumb.edu>
2019-03-27 02:28:21 +0000

    Lab 6

--- labs/lab6/dbConnection.php
+++ labs/lab6/dbConnection.php
@@ -1,25 +1,25 @@
 <?php
-function getDatabaseConnection($dbname = 'ottermart'){
-    $host = 'localhost';
-    $username ='root';
-    $password = '';
+
+function getDatabaseConnection($dbname = "ottermart") {
     
     
-        if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
+    $host = "localhost";
+    $username = "root";
+    $password = "";
+    
+   
+    if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
         $url = parse_url(getenv("mysql://be4028c0a5d88f:4840c9b0@us-cdbr-iron-east-03.cleardb.net/heroku_e668be4425805cc?reconnect=true"));
         $host = $url["host"];
         $dbname = substr($url["path"], 1);
         $username = $url["user"];
         $password = $url["pass"];
     } 
-
-    
-    
     
     $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    
-    $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
     
     return $dbConn;
 }
+
 ?>
